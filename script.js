3643416e765b904fa7f47fc061050564b2df8226
islam sani <islammuhd8@gmail.com>
2023-10-23 15:08:05 +0100

    added findMyKeys function

--- script.js
+++ script.js
@@ -171,7 +171,29 @@ function sortSpeciesByTeeth(arr) {
       return arr.sort(compareTeeth)
 }
 */
+console.log(sortSpeciesByTeeth(speciesArray));
 
-console.log(sortSpeciesByTeeth(speciesArray))
+
+// Write a function, findMyKeys(), that takes in an array of strings which may or may not contain 'keys'. If the keys are in the array, your function should return the index at which they can be found. If they’re not in the array, your function should return -1.
+
+const findMyKeys = arr => arr.findIndex(item => item === 'keys')
+
+/*
+// Alternate solution:
+// As a function declaration using a loop:
+function findMyKeys(arr) {
+      let index = -1;
+      for (let i = 0; i < arr.length; i++) {
+            if (arr[i] === 'keys') {
+                  index = i
+                  break
+            }
+      }
+      return index
+}
+*/
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff))
 
   
