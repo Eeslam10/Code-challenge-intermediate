2a1693dbda1779a66290384c7af284bbba3389a4
islam sani <islammuhd8@gmail.com>
2023-10-23 12:16:15 +0100

    added greetAlien function

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ const reverseArray = arr => {
 
 // The greeting should take the following format:
 // “Oh powerful [stringElement], we humans offer our unconditional surrender!”
+const aliens = ["Blorgous", "Glamyx", "Wegord", "SpaceKing"]
+
+const greetAliens = arr => {
+    for(let i = 0; i < arr.length; i++){
+      console.log(`Oh powerful ${arr[i]}, we humans offer our unconditional surrender!`)
+    }
+  }
+
+  greetAliens(aliens);
