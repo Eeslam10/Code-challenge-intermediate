98b6ffa419690217ba1b6c077968196e623823a5
islam sani <islammuhd8@gmail.com>
2023-10-23 14:58:13 +0100

    added isTheDinnerVegan function

--- script.js
+++ script.js
@@ -125,5 +125,27 @@ const justCoolStuff = (one, two) => {
   const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
   
   console.log(justCoolStuff(myStuff, coolStuff))
-  // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ]
+  // Should print [ 'fruit-by-the-foot', 'skateboards', 'my room' ];
+
+
+// Write a function isTheDinnerVegan() that takes in an array of food objects in the format:
+
+// {name: 'cabbage', source: 'plant' }
+
+// and returns a boolean value based on whether or not every item in the array has entirely plant-based origins.
+
+// const meal = [{name: 'arugula', source: 'plant'}, {name: 'tomatoes', source: 'plant'}, {name: 'lemon', source:'plant'}, {name: 'olive oil', source: 'plant'}];
+
+// isTheDinnerVegan(meal); // Should return true
+
+// Write your code here:
+const isTheDinnerVegan = arr => arr.every(food => food.source === 'plant')
+
+// Feel free to comment out the code below to test your function
+
+const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
+
+console.log(isTheDinnerVegan(dinner))
+// Should print false
+
   
